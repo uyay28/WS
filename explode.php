ef734c436e3830600a6357fc36ff2a06c3a99a85
siskafebrian044 <siskasisca1993@gmail.com>
2014-05-02 14:48:04 +0700

    Create explode.php

---
+++ explode.php
@@ -0,0 +1,48 @@
+<?php
+
+$fh = fopen("youtubeparse.txt", "r");
+
+while(!feof($fh)){
+	$current = trim(fgets($fh));
+	$iArray[] = explode("*", $current);
+}
+$count = count($iArray);
+for($x=0; $x<$count; $x++){
+	$newArray[$x]["title"] = $iArray[$x][0];
+	$newArray[$x]["link"] = $iArray[$x][1];
+	}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Youtube</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container">
+	<h1>Youtube StandUpComedyIndonesia SCRAPE</h1>
+	<?php
+	$output = '';
+	foreach($newArray as $new){
+		/*echo '<div class="video">';
+		echo '<img src ="' .$new['imageSrc'] . '" alt="youtube image">';
+		echo '<a href="' . $new['link'] . '"><h4>' . $new['title'] . '</h4></a>';*/
+
+	
+	echo '<table border="0" cellpadding="1" cellspacing="2">';
+	echo '<tr class="row">';
+	echo '<td class="col"><a href="' .$new['link']. '"><h5>' .$new['title']. '</h5></a></td>';
+	//echo '<td class="own"><a href="' .$new['ownerLink']. '"><h6>' .$new['owner']. '</h6></a></td>';
+	//cho '<td class="dur">' .$new['duration']. '</td>';
+	echo '</tr>';
+	echo '</table>';
+	}
+
+	?>
+	</div>
+</body>
+</html>
